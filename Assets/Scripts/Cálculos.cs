cb12867bd1d5432c25c02705859f022c05ca4f4b
50134830-sudo <50134830@est.ort.edu.ar>
2026-04-17 14:19:45 -0300

    Calculos

---
+++ Assets/Scripts/Cálculos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cálculos : MonoBehaviour
+{
+    public int Num1;
+    public int Num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int Resultado = Num1 + Num2;
+        Debug.Log("El resultado es igual a " + Resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
