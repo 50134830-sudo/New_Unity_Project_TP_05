9bc3812e46fb690b448887700ae1ee7c322375d6
50134830-sudo <50134830@est.ort.edu.ar>
2026-04-17 14:09:31 -0300

    Initial commit

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string Nombre;
+    public int Edad;
+    public float Estatura;
+    public bool EsDonante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es ", Nombre, ", tengo ", Edad, ", mido ", Estatura, " y es", EsDonante, " que soy donante de organos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/DeclaracionDeBariables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeBariables : MonoBehaviour
+{
+    public int Entero;       
+    public float Decimal;
+    public string Palabra;
+    public bool SiNo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Entero);
+        Debug.Log(Decimal);
+        Debug.Log(Palabra);
+        Debug.Log(SiNo);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
